fbbf2e74a8d80cb8cbccce8713d095bc095fafe0
Lakshya Rathore <rathorelakshya252@gmail.com>
2025-08-25 10:54:52 +0530

    Added contact form

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { Box } from '@mui/material'
+import ContactForm from '../../components/Home/Contact/ContactForm'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <Box sx={{
+      padding: { xs: '20px', md: '10px 50px' },
+    }}>
+      <ContactForm />
+    </Box>
   )
 }
 
